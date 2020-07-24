c0ca0ab62a9611941f6b4e9013c8cdf687dd1b49
percym <percymdev@gmail.com>
2020-07-24 18:55:22 +0200

    init commit

---
+++ bunny-jump/src/main.js
@@ -0,0 +1,10 @@
+
+import Phaser from './lib/phaser.js'
+ console.dir(Phaser)
+ console.log(Phaser)
+ console.log('hello')
+ export default new Phaser.Game({
+     type: Phaser.AUTO,
+     width: 480,
+     height: 640
+     })
