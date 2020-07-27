698c32735571251b07e2f3df22abead89c83c8af
percym <percymdev@gmail.com>
2020-07-27 11:40:31 +0200

    added scrolling platforms

--- bunny-jump/src/scenes/Game.js
+++ bunny-jump/src/scenes/Game.js
@@ -1,6 +1,11 @@
 import Phaser from '../lib/phaser.js'
 export default class Game extends Phaser.Scene{
 
+    /** @type {Phaser.physics.Arcade.sprite} 
+    player
+    /** @type {Phaser.physics.Arcade.staticGroup}  */
+    platforms
+
     constructor(){
         super('game')
     }
@@ -20,24 +25,53 @@ export default class Game extends Phaser.Scene{
         // this.add.image(240,320,'platform')
 
         // this.physics.add.image(240,320, 'platform').setScale(0.5) 
-        //note sprite
-       
-
-        const platforms = this.physics.add.staticGroup();
+      
+        this.platforms = this.physics.add.staticGroup();
 
         for (let i= 0; i < 5 ; i++){
             
             const x = Phaser.Math.Between(80,400)
             const y  = i * 150;
 
-            const platform = platforms.create(x,y, 'platform')
+            const platform = this.platforms.create(x,y, 'platform')
             platform.setScale(0.5)
 
-            const body = platform.body;
+            const body = platform.body
             body.updateFromGameObject()
-
         }
-        
-        this.physics.add.sprite(240, 320,'bunny-stand').setScale(0.5)
+      
+        //note sprite
+         this.player = this.physics.add.sprite(240, 320,'bunny-stand').setScale(0.5)
+         this.physics.add.collider(this.platforms, this.player);
+
+         this.player.body.checkCollision.up =false
+         this.player.body.checkCollision.left =false
+         this.player.body.checkCollision.right =false
+
+         this.cameras.main.startFollow(this.player);
+    }
+
+    update(){
+        this.platforms.children.iterate(
+            child => {
+                /** @type {Phaser.physics.Arcade.sprite} */
+                const platform = child
+                
+                const scrollY = this.cameras.main.scrollY
+                if(platform.y >= scrollY +700 ){
+                    platform.y = scrollY - Phaser.Math.Between(50,100)
+                    platform.body.updateFromGameObject()
+                }
+            }
+        )
+        const scrollY = this.cameras.main.scrollY;
+
+        const touchingDown = this.player.body.touching.down
+
+        if(touchingDown)(
+            this.player.setVelocity(-300)
+        )
+
+
     }
 }
