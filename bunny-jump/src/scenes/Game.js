1f8a8e48181416d937deaec0e8b4d4f8fb5ad7e3
percym <percymdev@gmail.com>
2020-07-28 19:02:03 +0200

    adding carrot

--- bunny-jump/src/scenes/Game.js
+++ bunny-jump/src/scenes/Game.js
@@ -6,6 +6,8 @@ export default class Game extends Phaser.Scene{
     /** @type {Phaser.physics.Arcade.staticGroup}  */
     platforms
 
+    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+    cursors
     constructor(){
         super('game')
     }
@@ -17,10 +19,14 @@ export default class Game extends Phaser.Scene{
         this.load.image('platform','assets/ground_grass.png')
 
         this.load.image('bunny-stand','assets/bunny1_stand.png')
+
+        this.cursors= this.input.keyboard.createCursorKeys()
+
     }
+   
 
     create(){
-        this.add.image(240,320, 'background').setScale(0.5)
+        this.add.image(240,320, 'background').setScrollFactor(1,0)
         
         // this.add.image(240,320,'platform')
 
@@ -34,7 +40,7 @@ export default class Game extends Phaser.Scene{
             const y  = i * 150;
 
             const platform = this.platforms.create(x,y, 'platform')
-            platform.setScale(0.5)
+            platform.scale=(0.5)
 
             const body = platform.body
             body.updateFromGameObject()
@@ -42,6 +48,7 @@ export default class Game extends Phaser.Scene{
       
         //note sprite
          this.player = this.physics.add.sprite(240, 320,'bunny-stand').setScale(0.5)
+
          this.physics.add.collider(this.platforms, this.player);
 
          this.player.body.checkCollision.up =false
@@ -49,6 +56,8 @@ export default class Game extends Phaser.Scene{
          this.player.body.checkCollision.right =false
 
          this.cameras.main.startFollow(this.player);
+
+         this.cameras.main.setDeadzone(this.scale.width * 1.5)
     }
 
     update(){
@@ -64,14 +73,34 @@ export default class Game extends Phaser.Scene{
                 }
             }
         )
-        const scrollY = this.cameras.main.scrollY;
 
         const touchingDown = this.player.body.touching.down
 
         if(touchingDown)(
             this.player.setVelocity(-300)
         )
+         
+        if(this.cursors.left.isDown && !touchingDown){
+            this.player.setVelocityX(-200)
+        }
+        else if(this.cursors.right.isDown && !touchingDown){
+            this.player.setVelocityX(200)
+        }else{
+            this.player.setVelocityX(0)
+        }
 
+        this.horizontalWrap(this.player)
+    }
 
+    horizontalWrap(sprite){
+        const halfWidth =sprite.displayWidth * 0.5 
+        const gameWidth = this.scale.width
+
+        if(sprite.x < -halfWidth){
+            sprite.x = gameWidth + halfWidth
+
+        }else if(sprite.x > gameWidth+ halfWidth) {
+            sprite.x = -halfWidth;
+        }
     }
 }
